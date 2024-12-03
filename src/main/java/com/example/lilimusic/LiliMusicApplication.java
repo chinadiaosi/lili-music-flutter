1376834213e2283d212cc1e7daa8468bcf24f11d
chinadiaosi <463284895@qq.com>
2024-12-03 16:19:48 +0800

    first commit

---
+++ src/main/java/com/example/lilimusic/LiliMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.lilimusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiliMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiliMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lilimusic/LiliMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lilimusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiliMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
